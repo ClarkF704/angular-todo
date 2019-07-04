cbd0dc18fc4a9c82ba2e2d58814ff340bd896898
Clark Farriss <clarkfarriss@Clarks-MacBook-Pro.local>
2019-07-04 19:31:48 -0400

    simple angular todo list

---
+++ src/app/components/todo-item/todo-item.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { TodoService } from '../../services/todo.service';
+
+import { Todo } from '../../models/Todo';
+
+@Component({
+  selector: 'app-todo-item',
+  templateUrl: './todo-item.component.html',
+  styleUrls: ['./todo-item.component.css']
+})
+export class TodoItemComponent implements OnInit {
+  @Input() todo:Todo
+  @Output() deleteTodo: EventEmitter<Todo> = new EventEmitter
+
+  constructor(private todoService:TodoService) { }
+
+  ngOnInit() {
+  }
+
+  // Set dynamic classes
+  setClasses(){
+    let classes = {
+      todo: true,
+      'is-complete': this.todo.completed
+    }
+    return classes;
+  }
+
+  onToggle(todo){
+    // Toggle UI
+
+    todo.completed = !todo.completed
+
+    // Toggle on Server
+    this.todoService.toggleCompleted(todo).subscribe(todo => {
+      console.log(todo)
+    })
+  }
+
+  onDelete(todo){
+    this.deleteTodo.emit(todo);
+  }
+
+}
